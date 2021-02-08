616cf3bce3c4d4178329370d5898c7d4fd069fa0
hakurakumasaki <hakurakumasaki@gmail.com>
2021-02-08 23:05:01 +0900

    as of 2021-02-08

--- Practice/weather/weather.py
+++ Practice/weather/weather.py
@@ -1,3 +1,5 @@
+#2月の気温から標本を抽出。95%信頼区間で検定する。
+
 import numpy as np
 import scipy as sp
 import pandas as pd
@@ -5,53 +7,40 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import csv
 from scipy import stats
+import datetime
+import random
 
-i = 0
-temp_diff = []
+
 pd.set_option('display.max_rows', None)
-comp_value = 0.5
+sns.set()
 
-#csvのデータをリストとして読み込み
-with open('./data_weather.csv', 'r') as f:
-    data = list(csv.reader(f))
 
-#日次差分データをDataFrameに格納。
-df = pd.DataFrame(data, columns = ['date', 'low_temp', 'high_temp', 'ave_temp'])
-df = df.set_index('date').drop('date').astype('float64')#リスト先頭のラベルを行タイトルにし、当該データを削除。データの型をstrからfloatに変換。
-df_diff = df.diff(1)#前日とのデータの差分を取得
+df = pd.read_csv('./data_weather.csv', index_col ='date')
+df.index = pd.to_datetime(df.index, format = '%Y-%m-%d')#日付を0で桁埋めするフォーマット。
+df = df.astype('float64')#データの型をstrからfloatに変換。
 
-#統計量を計算
-high_temp_diff = df_diff['high_temp'].drop(('2011/1/1'))
-mu = np.mean(high_temp_diff)
-n = len(high_temp_diff)
-DoF = n -1
-sigma = np.std(high_temp_diff, ddof = 1)#正規分布を仮定。不偏分散の場合はstdではなくvar
-se = sigma/np.sqrt(n)
-t_value = (mu - comp_value) / se
-alpha = stats.t.cdf(np.abs(t_value), df = DoF)
-p_value = (1 - alpha) * 2
-lo, up = stats.t.interval(alpha = 0.95, df = DoF, loc = mu, scale = se)
+#2月のデータだけを抽出
+df = df[df.index.month == 2]['low_temp']
+#df = df['low_temp']#2月の最低気温だけをデータとして持つDataFrameを定義
 
-#print(high_temp_diff)
-#high_temp_diff.to_csv('output.csv')
-#統計量を出力して確認
-print(high_temp_diff.head())
-print("mu:",mu)
-print("DoF:",DoF)
-print("se:",se)
-print("alpha:",alpha)
-print('t_value:',t_value)
-print("p_value:", p_value)
-print("low:",lo)
-print("up:",up)
+#過去10年の2月の最低気温の分布を確認
+mu_a = np.mean(df)#aはallの略
+n_a = len(df)
+dof_a = n_a -1
+#sns.histplot(df_l, kde = True)
+#plt.show()
 
-'''
-if p_value < 0.05:
-    print('meaningfully different')
-else:
-    print('not meaningfully different')
-'''
+#過去10年の2月の最低気温の中からランダムに10日分のデータを取得
+df_s = df.sample(n=10)#sはsampleの略
 
-#グラフ描画　←　必要な時だけコメントアウトする
-#sns.histplot(high_temp_diff, kde=True)
+#サンプルについて統計量を計算
+n_s = len(df_s)
+dof_s = n_s -1
+mu_s = np.mean(df_s)
+se_s = np.std(df_s, ddof = 1) / np.sqrt(n_s)
+interval = stats.t.interval(alpha = 0.95, df = dof_s, loc = mu_s, scale = se_s)
+print(interval)
+
+#sns.histplot(df, kde=True)
+#sns.histplot(df_s, kde = True)
 #plt.show()
